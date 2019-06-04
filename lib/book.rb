41a3158b6529299a36138aca0affb6ce3be743d1
Francis Mendez <fmendez884@gmail.com>
2019-06-04 23:56:53 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -9,7 +9,36 @@ class Book
   end
   
   def author=(author)
-    
+    @author = author
   end
   
+  def author
+    @author
+  end
+  
+  def page_count=(page_count)
+    @page_count = page_count
+  end 
+  
+  def page_count
+    @page_count
+  end
+  
+  def genre=(genre)
+    @genre = genre
+  end
+  
+  def genre 
+    @genre
+  end
+  
+  def turn_page
+    puts "F"
+  end 
+  
 end
+
+
+
+
+
