9b78e0eff09b72930469c2cb8e9a6376f38526d4
Francis Mendez <fmendez884@gmail.com>
2019-06-05 00:07:22 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,11 +3,14 @@
 class Shoe 
   attr_accessor :color, :size, :material 
   attr_reader :brand 
+  attr_writer :condition
   
   def initialize(brand)
     @brand = brand
-end
+  end
 
-
+  def condition(condition)
+    
+  end 
 
 end 
